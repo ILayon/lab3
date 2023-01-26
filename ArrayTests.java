f7c98a1ba48e070066f5a7e95d64f1f02f981f4c
ILayon <122569292+ILayon@users.noreply.github.com>
2023-01-26 12:54:28 -0800

    Update lab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlaceWithMoreElements() {
+    int[] input1 = { 1, 2, 3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3, 2, 1 }, input1);
+	}
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedWithMoreElements() {
+    int[] input1 = { 4, 5, 6 };
+    assertArrayEquals(new int[]{ 6, 5, 4 }, ArrayExamples.reversed(input1));
   }
 }
